a55e1cf9e923819d2e8e5ca4e46dcb8b4fc10792
Ilya Kazakov <robotbotyourube@gmail.com>
2025-03-04 21:26:12 +0300

    Initial commit

---
+++ src/main/java/com/robotbot/finance_tracker_server/FinanceTrackerServerApplication.java
@@ -0,0 +1,13 @@
+package com.robotbot.finance_tracker_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceTrackerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceTrackerServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/robotbot/finance_tracker_server/FinanceTrackerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robotbot.finance_tracker_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceTrackerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
