5a0399728e04fa1b488acf47b85d95f818622fdd
Ilya Kazakov <robotbotyourube@gmail.com>
2025-03-06 17:12:32 +0300

    Add prepopulate icons

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO icons (id, name, path) VALUES (1, 'Bank', '/icons/account_bank_24dp.svg');
+INSERT INTO icons (id, name, path) VALUES (2, 'Wallet', '/icons/account_wallet_24dp.svg');
+INSERT INTO icons (id, name, path) VALUES (3, 'Card', '/icons/account_card_24dp.svg');
+INSERT INTO icons (id, name, path) VALUES (4, 'Bus', '/icons/category_bus_24dp.svg');
